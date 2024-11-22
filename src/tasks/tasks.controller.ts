3a087b612b9724ecebb6e5848487bf3fdbf2e598
unknown <diegopablo012@gmail.com>
2024-11-22 11:32:06 -0600

    feat: crated HTTPS methods for tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -9,6 +9,26 @@ export class TasksController {
   @Get()
   getTasks() {
     return this.tasksService.getTasks();
+  }
+
+  @Post()
+  createTask() {
+    return this.tasksService.createTask();
+  }
+
+  @Put()
+  updateTask() {
+    return this.tasksService.updateTask();
+  }
+
+  @Patch()
+  patchTaskStatus() {
+    return this.tasksService.patchTaskStatus();
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTask();
   }
 
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -7,4 +7,20 @@ export class TasksService {
     return ['Task 1', 'Task 2', 'Task 3'];
   }
 
+  createTask() {
+    return 'Task created';
+  }
+
+  updateTask() {
+    return 'Task updated';
+  }
+
+  patchTaskStatus(){
+    return 'Task status updated';
+  }
+
+  deleteTask() {
+    return 'Task deleted';
+  } 
+
 }
