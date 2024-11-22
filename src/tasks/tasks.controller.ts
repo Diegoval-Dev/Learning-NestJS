c52371a2fa414fff8700a8c96d4806cf5f310fc2
unknown <diegopablo012@gmail.com>
2024-11-22 11:17:50 -0600

    feat: create services by command line

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  tasksService: TasksService;
+
+  constructor(taskService: TasksService) {
+    this.tasksService = taskService
+  }
+
+  @Get()
+  getTasks() {
+    return this.tasksService.getTasks();
+  }
+
+}
