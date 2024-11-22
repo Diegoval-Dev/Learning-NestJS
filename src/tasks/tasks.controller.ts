6eafef0267792bcf237b330a611bdcd081ffe20f
unknown <diegopablo012@gmail.com>
2024-11-22 12:14:14 -0600

    feat: add query to get

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post, Put, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { query } from 'express';
 
 @Controller('tasks')
 export class TasksController {
@@ -7,12 +8,17 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks() {
+  getTasks(
+    @Query() query: any
+  ) {
+    console.log(query);
     return this.tasksService.getTasks();
   }
 
   @Post()
-  createTask(@Body() task: any) {
+  createTask(
+    @Body() task: any
+  ) {
     return this.tasksService.createTask(task);
   }
 
